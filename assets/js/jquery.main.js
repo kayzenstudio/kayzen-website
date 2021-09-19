1190ab0039d6e54c6b6b8d709a9e341813491734
Mauricio Vuljevas <hallo@mvuljevas.com>
2021-09-19 15:42:08 -0300

    Contact Form & Responsive CSS
    - Contact Form Integration
    - Contact Form CSS
    - Minor Responsive Tweaks

--- assets/js/jquery.main.js
+++ assets/js/jquery.main.js
@@ -1,5 +1,13 @@
-$('#show-form').on('click', function (e) {
+// Show and Hide Form Handles
+$("#show-form").on("click", function (e) {
   e.preventDefault();
-  $('#form-wrapper').addClass('active');
+  $("#form-wrapper").addClass("active");
+  $(".close-overlay").removeClass("hide");
 });
 
+$(".close-overlay").on("click", function (e) {
+  e.preventDefault();
+  $(this).addClass("hide");
+  $("#form-wrapper").removeClass("active");
+  console.log("hide form");
+});
